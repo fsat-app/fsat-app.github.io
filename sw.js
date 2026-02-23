c387b1027a6bb625f8ecae7620927bfb3d2f1324
fsat-app <fsat-hug@proton.me>
2026-02-23 13:26:56 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,67 +1,66 @@
 // sw.js (Service Worker)
 const CACHE_NAME = 'aquarelle-pwa-cache-v65';
-const ROOT = "/"
 const urlsToCache = [
-   ROOT,
-   ROOT + 'index.html',
-   ROOT + 'lib/dexie.min.js',
-   ROOT + 'lib/sweetalert2@11.js',
-   ROOT + 'lib/aos.min.js',
-   ROOT + 'lib/xlsx.full.min.js',
-   ROOT + 'lib/chart.min.js',
-   ROOT + 'app/fsat.min.js',
-   ROOT + 'manifest.json',
-   ROOT + 'css/aos.min.css',
-   ROOT + 'css/custom.css',
-   ROOT + 'css/pico.min.css',
-   ROOT + 'font/style.css',
-   ROOT + 'css/font/Zain-ExtraLight.ttf',
-   ROOT + 'css/font/Zain-Light.ttf',
-   ROOT + 'css/font/Zain-Regular.ttf',
-   ROOT + 'css/font/Zain-Bold.ttf',
-   ROOT + 'css/font/Zain-ExtraBold.ttf',
-   ROOT + 'css/font/Zain-Black.ttf',
-   ROOT + 'css/font/Zain-LightItalic.ttf',
-   ROOT + 'css/font/Zain-Italic.ttf',
-   ROOT + 'img/logos/hug-white.png',
-   ROOT + 'img/logos/fmed-white.png',
-   ROOT + 'img/logos/cc.png',
-   ROOT + 'img/logos/fsat.jpg',
-   ROOT + 'img/graphics/body-bg-top.jpg',
-   ROOT + 'img/graphics/body-fig-1.jpg',
-   ROOT + 'img/graphics/body-fig-2w.png',
-   ROOT + 'img/graphics/body-fig-2y.png',
-   ROOT + 'img/graphics/body-fig-3.png',
-   ROOT + 'img/graphics/hero.png',
-   ROOT + 'img/graphics/popup-brush.png',
-   ROOT + 'img/graphics/popup-fig-1.jpg',
-   ROOT + 'img/graphics/popup-fig-2.jpg',
-   ROOT + 'img/gallery/1A.jpg',
-   ROOT + 'img/gallery/1B.jpg',
-   ROOT + 'img/gallery/2A.jpg',
-   ROOT + 'img/gallery/2B.jpg',
-   ROOT + 'img/gallery/2C.jpg',
-   ROOT + 'img/gallery/3B.jpg',
-   ROOT + 'img/gallery/NM1.jpg',
-   ROOT + 'img/gallery/unknown.jpg',
-   ROOT + 'img/gallery-l/1A.jpg',
-   ROOT + 'img/gallery-l/1B.jpg',
-   ROOT + 'img/gallery-l/2A.jpg',
-   ROOT + 'img/gallery-l/2B.jpg',
-   ROOT + 'img/gallery-l/2C.jpg',
-   ROOT + 'img/gallery-l/3B.jpg',
-   ROOT + 'img/oms/I.jpg',
-   ROOT + 'img/oms/II.jpg',
-   ROOT + 'img/oms/III.jpg',
-   ROOT + 'img/oms/NM.jpg',
-   ROOT + 'asset/countries-ISO-3166.json',
-   ROOT + 'fav/apple-touch-icon.png',
-   ROOT + 'fav/favicon.ico',
-   ROOT + 'fav/favicon.svg',
-   ROOT + 'fav/favicon-96x96.png',
-   ROOT + 'fav/site.webmanifest',
-   ROOT + 'fav/web-app-manifest-192x192.png',
-   ROOT + 'fav/web-app-manifest-512x512.png'
+   '/',
+  '/index.html',
+  '/lib/dexie.min.js',
+  '/lib/sweetalert2@11.js',
+  '/lib/aos.min.js',
+  '/lib/xlsx.full.min.js',
+  '/lib/chart.min.js',
+  '/app/fsat.min.js',
+  '/manifest.json',
+  '/css/aos.min.css',
+  '/css/custom.css',
+  '/css/pico.min.css',
+  '/font/style.css',
+  '/css/font/Zain-ExtraLight.ttf',
+  '/css/font/Zain-Light.ttf',
+  '/css/font/Zain-Regular.ttf',
+  '/css/font/Zain-Bold.ttf',
+  '/css/font/Zain-ExtraBold.ttf',
+  '/css/font/Zain-Black.ttf',
+  '/css/font/Zain-LightItalic.ttf',
+  '/css/font/Zain-Italic.ttf',
+  '/img/logos/hug-white.png',
+  '/img/logos/fmed-white.png',
+  '/img/logos/cc.png',
+  '/img/logos/fsat.jpg',
+  '/img/graphics/body-bg-top.jpg',
+  '/img/graphics/body-fig-1.jpg',
+  '/img/graphics/body-fig-2w.png',
+  '/img/graphics/body-fig-2y.png',
+  '/img/graphics/body-fig-3.png',
+  '/img/graphics/hero.png',
+  '/img/graphics/popup-brush.png',
+  '/img/graphics/popup-fig-1.jpg',
+  '/img/graphics/popup-fig-2.jpg',
+  '/img/gallery/1A.jpg',
+  '/img/gallery/1B.jpg',
+  '/img/gallery/2A.jpg',
+  '/img/gallery/2B.jpg',
+  '/img/gallery/2C.jpg',
+  '/img/gallery/3B.jpg',
+  '/img/gallery/NM1.jpg',
+  '/img/gallery/unknown.jpg',
+  '/img/gallery-l/1A.jpg',
+  '/img/gallery-l/1B.jpg',
+  '/img/gallery-l/2A.jpg',
+  '/img/gallery-l/2B.jpg',
+  '/img/gallery-l/2C.jpg',
+  '/img/gallery-l/3B.jpg',
+  '/img/oms/I.jpg',
+  '/img/oms/II.jpg',
+  '/img/oms/III.jpg',
+  '/img/oms/NM.jpg',
+  '/asset/countries-ISO-3166.json',
+  '/fav/apple-touch-icon.png',
+  '/fav/favicon.ico',
+  '/fav/favicon.svg',
+  '/fav/favicon-96x96.png',
+  '/fav/site.webmanifest',
+  '/fav/web-app-manifest-192x192.png',
+  '/fav/web-app-manifest-512x512.png'
 ];
 self.addEventListener('install', event => {
     event.waitUntil(
@@ -96,8 +95,8 @@ self.addEventListener('fetch', event => {
     let requestUrl = new URL(event.request.url);
     let cacheKey = requestUrl.pathname;
     // Normalise la racine
-    if (cacheKey === ROOT || cacheKey === ROOT.slice(0, -1)) {
-        cacheKey = ROOT + 'index.html';
+    if (cacheKey === '/') {
+       cacheKey ='/index.html';
     }
     event.respondWith(
         caches.match(cacheKey)
@@ -106,9 +105,10 @@ self.addEventListener('fetch', event => {
                     return response;
                 }
                 return fetch(event.request).catch(() => {
-                    return caches.match(ROOT + 'index.html');
+                    return caches.match('/index.html');
                 });
             })
     );
 });
+
 
